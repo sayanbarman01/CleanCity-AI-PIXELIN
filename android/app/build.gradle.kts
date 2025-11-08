a0abeb9099e35ece6a928c397559932585e464e3
Sayan Barman <sayanbarman634@gmail.com>
2025-11-08 12:23:46 +0530

    fix error in apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cleancity_ai_pixelin"
+    namespace = "com.example.cleancityai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cleancity_ai_pixelin"
+        applicationId = "com.example.cleancityai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -46,11 +46,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "cleancity_ai_pixelin");
+    gtk_header_bar_set_title(header_bar, "cleancityai");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "cleancity_ai_pixelin");
+    gtk_window_set_title(window, "cleancityai");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
